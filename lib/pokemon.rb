332b8487750f8fc33f4b40bf4874dba04a50cfd8
Kevin Boston <kbos22@gmail.com>
2020-07-07 16:36:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ class Pokemon
     SELECT * FROM pokemon WHERE id = ?
     LIMIT 1
     SQL
-    new_poke = db.execute(sql, id_num)
+    new_poke = db.execute(sql, id_num).flatten
     Pokemon.new(id: new_poke[0], name: new_poke[1], type: new_poke[2], hp: new_poke[3], db: db)
   end
 
